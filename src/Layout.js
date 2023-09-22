e15fd6c698acdb8f9e458f6a47541f491672fee4
Nicholas Taveras <taveras123n@gmail.com>
2023-09-22 02:05:56 -0400

    Removed feedback and business pages

--- src/Layout.js
+++ src/Layout.js
@@ -1,22 +1,22 @@
 // Styling
-import styled from 'styled-components';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MediaQuery from 'react-responsive';
+import styled from "styled-components";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MediaQuery from "react-responsive";
 
 // component imports
-import Header from './components/Header';
-import Footer from './components/Footer';
-import SideBar from './components/SideBar';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
 
 // page imports
-import HomePage from './pages/HomePage';
-import ContactPage from './pages/ContactPage';
-import PortfolioPage from './pages/PortfolioPage';
-import AboutMePage from './pages/AboutMePage';
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import PortfolioPage from "./pages/PortfolioPage";
+import AboutMePage from "./pages/AboutMePage";
 
 // React Imports
-import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 // Styled Component
 const StyledLayout = styled.div`
@@ -24,7 +24,7 @@ const StyledLayout = styled.div`
   display: flex;
   flex-direction: column;
   main {
-  flex-grow: 1;
+    flex-grow: 1;
   }
 `;
 
@@ -35,58 +35,44 @@ function Layout() {
         <Header />
       </MediaQuery>
       <MediaQuery maxDeviceWidth={692}>
-        <SideBar/>
+        <SideBar />
       </MediaQuery>
-        <main>
-          <Switch>
-            <Route exact path='/' render={props =>
-              <HomePage 
-              {...props}
-              />
-            } />
-            <Route exact path='/contact' render={props =>
-              <ContactPage
-              {...props}
-              />
-            } />
-            <Route exact path='/portfolio' render={props =>
-              <PortfolioPage
-              {...props}
-              />
-            } />
-            <Route exact path='/portfolio/marvel-character-selector' render={props =>
-              <PortfolioPage
-              {...props}
-              />
-            } />
-            <Route exact path='/portfolio/diet-tracker' render={props =>
-              <PortfolioPage
-              {...props}
-              />
-            } />
-            <Route exact path='/portfolio/league-elite' render={props =>
-              <PortfolioPage
-              {...props}
-              />
-            } />
-            <Route exact path='/aboutme' render={props =>
-              <AboutMePage
-              {...props}
-              />
-            } />
-            <Route exact path='/contact/feedback' render={props =>
-              <ContactPage
-              {...props}
-              />
-            } />
-            <Route exact path='/contact/business' render={props =>
-              <ContactPage
-              {...props}
-              />
-            } />
-          </Switch>
-        </main>
-      <Footer/>
+      <main>
+        <Switch>
+          <Route exact path="/" render={(props) => <HomePage {...props} />} />
+          <Route
+            exact
+            path="/contact"
+            render={(props) => <ContactPage {...props} />}
+          />
+          <Route
+            exact
+            path="/portfolio"
+            render={(props) => <PortfolioPage {...props} />}
+          />
+          <Route
+            exact
+            path="/portfolio/marvel-character-selector"
+            render={(props) => <PortfolioPage {...props} />}
+          />
+          <Route
+            exact
+            path="/portfolio/diet-tracker"
+            render={(props) => <PortfolioPage {...props} />}
+          />
+          <Route
+            exact
+            path="/portfolio/league-elite"
+            render={(props) => <PortfolioPage {...props} />}
+          />
+          <Route
+            exact
+            path="/aboutme"
+            render={(props) => <AboutMePage {...props} />}
+          />
+        </Switch>
+      </main>
+      <Footer />
     </StyledLayout>
   );
 }
--- src/components/SideBarData/index.js
+++ src/components/SideBarData/index.js
@@ -1,56 +1,42 @@
-import React from 'react'
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
-import * as RiIcons from 'react-icons/ri';
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+import * as RiIcons from "react-icons/ri";
 
 export const SideBarData = [
-    {
-        title: 'Portfolio',
-        path: '/portfolio',
-        icon: <AiIcons.AiFillFolder />,
-        iconClosed: <RiIcons.RiArrowDownSFill />,
-        iconOpened: <RiIcons.RiArrowUpSFill />,
-        subNav: [
-            {
-                title: 'League Elite',
-                path: '/portfolio/league-elite',
-                icon: <AiIcons.AiFillFile />
-            },
-            {
-                title: 'Diet Tracker',
-                path: '/portfolio/diet-tracker',
-                icon: <AiIcons.AiFillFile />
-            },
-            {
-                title: 'Marvel Selector',
-                path: '/portfolio/marvel-character-selector',
-                icon: <AiIcons.AiFillFile />
-            },
-        ]
-    },
-    {
-        title: 'About Me',
-        path: '/aboutme',
-        icon: <AiIcons.AiFillPhone />
-    },
-    {
-        title: 'Contact Info',
-        path: '/contact',
-        icon: <AiIcons.AiFillPhone />,
-        iconClosed: <RiIcons.RiArrowDownSFill />,
-        iconOpened: <RiIcons.RiArrowUpSFill />,
-        subNav: [
-            {
-                title: 'Feedback',
-                path: '/contact/feedback',
-                icon: <IoIcons.IoIosPaperPlane />
-            },
-            {
-                title: 'Business',
-                path: '/contact/business',
-                icon: <IoIcons.IoIosPaperPlane />
-            },
-        ]
-    },
-]
+  {
+    title: "Portfolio",
+    path: "/portfolio",
+    icon: <AiIcons.AiFillFolder />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+    subNav: [
+      {
+        title: "League Elite",
+        path: "/portfolio/league-elite",
+        icon: <AiIcons.AiFillFile />,
+      },
+      {
+        title: "Diet Tracker",
+        path: "/portfolio/diet-tracker",
+        icon: <AiIcons.AiFillFile />,
+      },
+      {
+        title: "Marvel Selector",
+        path: "/portfolio/marvel-character-selector",
+        icon: <AiIcons.AiFillFile />,
+      },
+    ],
+  },
+  {
+    title: "About Me",
+    path: "/aboutme",
+    icon: <AiIcons.AiFillPhone />,
+  },
+  {
+    title: "Contact Info",
+    path: "/contact",
+    icon: <AiIcons.AiFillPhone />,
+  },
+];

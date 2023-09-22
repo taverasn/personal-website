d95fecae200bdb18fadefb0eda946172bde00a00
Nicholas Taveras <taveras123n@gmail.com>
2023-09-22 02:05:36 -0400

    Updated About me page to contain some info about me

---
+++ src/components/AboutMeData/index.js
@@ -0,0 +1,50 @@
+const AboutMeData = (props) => {
+  return (
+    <main>
+      <p>
+        Welcome to my portfolio! I'm a dedicated Game Developer with a solid
+        foundation in engineering, and I'm thrilled to share my journey with
+        you.
+      </p>
+      <p>
+        I bring three years of professional engineering experience to the table,
+        during which I've had the opportunity to work on a diverse range of
+        projects, including websites, games, and Windows applications. This
+        hands-on experience has been invaluable in shaping my skills and
+        perspective as a developer.
+      </p>
+      <p>
+        My coding journey took an exciting turn when I embarked on a coding boot
+        camp in software engineering, which I completed from September 2020 to
+        March 2021. During this intensive program, I dove deep into the world of
+        coding, mastering JavaScript, HTML, CSS, Python, React, Django, Trello,
+        PostgreSQL, and MongoDB. This experience has equipped me with a
+        versatile skill set that allows me to approach projects from various
+        angles.
+      </p>
+      <p>
+        I'm also in the final stretch of my Bachelor's in Game Development, a
+        journey that began in June 2021 and will culminate in December 2023.
+        Throughout my academic career, I've had the privilege of working on a
+        wide range of projects, both solo and in collaboration with talented
+        peers. In group settings, I often took on leadership roles, leveraging
+        my skills to guide and inspire my teammates. I'm proud of my ability to
+        review and optimize code, as well as my knack for tackling challenges
+        with minimal guidance. Constructive criticism has been a vital part of
+        my growth, and I eagerly embrace opportunities for learning and
+        improvement.
+      </p>
+      <p>
+        My project portfolio reflects my versatility and dedication. Whether
+        it's crafting websites, developing Windows applications, or creating
+        immersive games, I thrive on bringing creative visions to life through
+        code. Currently, I'm actively seeking an exciting role in Game
+        Development. I'm enthusiastic about collaborating with like-minded
+        professionals, pushing the boundaries of what's possible in the gaming
+        industry, and contributing my skills and passion to innovative projects.
+      </p>
+    </main>
+  );
+};
+
+export default AboutMeData;
--- src/pages/AboutMePage/index.js
+++ src/pages/AboutMePage/index.js
@@ -1,7 +1,9 @@
 // Styling Imports
-import styled from 'styled-components';
+import styled from "styled-components";
+import MediaQuery from "react-responsive";
 
 // Component Imports
+import AboutMeData from "../../components/AboutMeData";
 
 // Styled Components
 const StyledPage = styled.main`
@@ -12,17 +14,41 @@ const StyledPage = styled.main`
   align-items: center;
   background-color: #232323;
   color: black;
-  h1 {
-      color: black;
+  p {
+    margin: 5px 5em 5px;
+    font-size: 20px;
+  }
+`;
+
+const StyledMobilePage = styled.main`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background-color: #232323;
+  color: black;
+  p {
+    margin: 5px 3em 5px;
+    font-size: 16px;
   }
 `;
 
 const AboutMePage = (props) => {
-    return (
+  return (
+    <main>
+      <MediaQuery minDeviceWidth={693}>
         <StyledPage>
-          <h1>AboutMePage</h1>
+          <AboutMeData />
         </StyledPage>
-    )
-}
+      </MediaQuery>
+      <MediaQuery maxDeviceWidth={692}>
+        <StyledMobilePage>
+          <AboutMeData />
+        </StyledMobilePage>
+      </MediaQuery>
+    </main>
+  );
+};
 
 export default AboutMePage;

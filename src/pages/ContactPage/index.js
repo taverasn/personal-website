2768559dffd67c71fc64afccd53be6fe72b24448
Nicholas Taveras <taveras123n@gmail.com>
2023-09-22 02:06:08 -0400

    Added Contact info to the contact page

--- src/pages/ContactPage/index.js
+++ src/pages/ContactPage/index.js
@@ -1,5 +1,5 @@
 // Styling Imports
-import styled from 'styled-components';
+import styled from "styled-components";
 
 // Component Imports
 
@@ -7,21 +7,43 @@ import styled from 'styled-components';
 const StyledPage = styled.main`
   min-height: 100vh;
   display: flex;
-  justify-content: center;
   flex-direction: column;
   align-items: center;
   background-color: #232323;
-  h1 {
-      color: black;
-  }
+  color: black;
+  padding-top: 10px;
 `;
 
 const ContactPage = (props) => {
-    return (
-        <StyledPage>
-          <h1>ContactPage</h1>
-        </StyledPage>
-    )
-}
+  return (
+    <StyledPage>
+      <h1>Contact Details</h1>
+      <section>
+        <ul>
+          <li>
+            <strong>Name:</strong> Nicholas Taveras
+          </li>
+          <li>
+            <strong>Email:</strong>{" "}
+            <a href="mailto:taveras123n@gmail.com">taveras123n@gmail.com</a>
+          </li>
+          <li>
+            <strong>Phone:</strong> +1 (484) 651-2374
+          </li>
+          <li>
+            <strong>Location:</strong> Reading, Pennsylvania
+          </li>
+        </ul>
+      </section>
+
+      <h1>Availability</h1>
+      <section>
+        <p>Monday - Wednesday: 9:00 AM - 6:00 PM EST</p>
+        <p>Thursday: Unavailable</p>
+        <p>Friday - Sunday: 9:00 AM - 10:00 PM EST</p>
+      </section>
+    </StyledPage>
+  );
+};
 
 export default ContactPage;

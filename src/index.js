c7242bb9e50a343b5ce041b384cce0a6be523840
Nicholas Taveras <taveras123n@gmail.com>
2023-09-22 09:43:35 -0400

    Refreshing on pages that are not the home page no longer give a 404

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Layout from './Layout';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Layout from "./Layout";
+import reportWebVitals from "./reportWebVitals";
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/personal-website">
       <Layout />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

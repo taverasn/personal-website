40c5d38c95439efb902952983d6e7cd2074e0614
Nicholas Taveras <taveras123n@gmail.com>
2023-12-04 19:06:43 -0500

    Updated Project Links

--- src/components/PortfolioData/index.js
+++ src/components/PortfolioData/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import * as GiIcons from "react-icons/gi";
 export const PortfolioData = [
   {
-    title: "Psyche",
-    duration: "September 2023 - Present",
+    title: "Sad Ogre Studios - Psyche",
+    duration: "August 2023 - Present",
     role: "UI/Systems Programmer",
     path: "/portfolio/psyche",
     skills: "Unity | C# | Jira | Confluence | Perforce | FMOD",
-    projectLink:
-      "https://drive.google.com/file/d/1mkkOS9oaxftEUA3xvfr0FxtB0V4p9MoJ/view?usp=sharing",
+    projectLink: "https://sadogrestudios.itch.io/psyche",
     icon: <GiIcons.GiBrain />,
     image1: "/Psyche_Images/psyche_gameplay.png",
     contributions: [
       {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
         name: "Robust Save and Load System",
         description:
           "I implemented a robust save and load system, allowing players to effortlessly save their progress and resume their journey at any point. This feature enhances player convenience and engagement.",
@@ -20,12 +20,12 @@ export const PortfolioData = [
       {
         name: "User-Friendly Pause Menu",
         description:
-          " I designed a user-friendly pause menu, providing players with options to adjust sound, video, and control settings on the fly. This ensures players have control over their gaming experience.",
+          " I designed a user-friendly pause menu, providing players with options to adjust audio and video settings on the fly. This ensures players have control over their gaming experience.",
       },
       {
         name: "Comprehensive Player Menu",
         description:
-          "I created a comprehensive player menu that includes class selection, achievements, memories, and a quest journal. This menu serves as a valuable resource for players, enhancing their understanding of the game world.",
+          "I created a comprehensive player menu that includes class selection, memories, and a quest journal. This menu serves as a valuable resource for players, enhancing their understanding of the game world.",
       },
       {
         name: "HUD Design",
@@ -47,16 +47,20 @@ export const PortfolioData = [
         description:
           "Utilizing Unity, I designed visually appealing and intuitive user interfaces for both the player menu and pause menu, enhancing the overall user experience.",
       },
+      {
+        name: "Quest System Design",
+        description:
+          "Designed and implemented an immersive Quest System, merging quest points, map, and journal for seamless player navigation and organization. Introduced multi-step quests with varied states and rewards, crafting a dynamic narrative that captivates players through real-time notifications.",
+      },
     ],
   },
   {
-    title: "Descender",
+    title: "Galaxy Cabin - Descender",
     duration: "May 2023 - May 2023",
     role: "Graphics Programmer",
     path: "/portfolio/descender",
     skills: "C++ | Flecs | Jira | Github | OpenGL | Blender | Freetype",
-    projectLink:
-      "https://drive.google.com/file/d/1oxzMREAeszZtxaEEQOaywoJkmjTzFz67/view?usp=drive_link",
+    projectLink: "https://phorcy.itch.io/descender-release-build",
     icon: <GiIcons.GiSpaceship />,
     image1: "/Descender_Images/descender_gameplay.png",
     contributions: [
@@ -88,12 +92,12 @@ export const PortfolioData = [
     ],
   },
   {
-    title: "Thiefs Fortune",
+    title: "Prime Androids - Thiefs Fortune",
     duration: "August 2022 - November 2022",
     role: "Lead/Gameplay/Systems Programmer",
     path: "/portfolio/thiefs-fortune",
     skills: "Unity | C# | Trello | Github",
-    projectLink: "https://github.com/taverasn/AnubisRevenge/tree/main",
+    projectLink: "https://phorcy.itch.io/thiefs-fortune-demo",
     icon: <GiIcons.GiPirateFlag />,
     image1: "/ThiefsFortune_Promo/Shop.png",
     contributions: [
@@ -130,7 +134,7 @@ export const PortfolioData = [
     role: "Lead/UI/Gameplay Programmer",
     path: "/portfolio/anubis-revenge",
     skills: "Unity | C# | Trello | Github",
-    projectLink: "https://github.com/taverasn/AnubisRevenge/tree/main",
+    projectLink: "https://phorcy.itch.io/anubis-revenge-demo",
     icon: <GiIcons.GiMummyHead />,
     image1: "/Anubis_Images/anubis_main.png",
     contributions: [
--- src/pages/ProjectPage/index.js
+++ src/pages/ProjectPage/index.js
@@ -1,6 +1,7 @@
 // Styling Imports
 import styled from "styled-components";
 import MediaQuery from "react-responsive";
+import ReactPlayer from "react-player/youtube";
 
 // Component Imports
 
@@ -22,12 +23,31 @@ const StyledPage = styled.main`
   hr {
     background: yellow;
     height: 1px;
-    width: 50%;
+    width: 100%;
   }
   img {
     height: 25%;
     width: 50%;
     margin: 5px 50% 5px;
+  }
+  h3,
+  description {
+    text-align: center;
+  }
+  .player-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0;
+    margin: 0;
+  }
+  .react-player {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  img {
+    padding-bottom: 10px;
   }
 `;
 
@@ -41,19 +61,33 @@ const StyledMobilePage = styled.main`
   padding-top: 20px;
   padding-bottom: 20px;
   div {
-    padding-top: 15px;
-    margin-top: 15px;
+    text-align: center;
     margin: 5px 5px 5px;
   }
   hr {
     background: yellow;
     height: 1px;
     width: 95%;
+    margin-left: 10px;
   }
   img {
     height: 25%;
     width: 95%;
     margin: 5px 50% 5px;
+    padding-bottom: 10px;
+  }
+  .player-wrapper {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+  }
+  .description {
+    width: 95%;
+    margin-left: 10px;
+  }
+  .react-player {
+    padding-bottom: 10px;
   }
 `;
 
@@ -80,16 +114,23 @@ const ProjectPage = ({ item }) => {
             <p>{item.duration}</p>
           </StyledDiv>
           <p>{item.skills}</p>
-          <hr />
           <img src={item.image1} alt="Main Project Photo" />
-          <h2>Contributions</h2>
-          <hr />
 
           {item.contributions.map((item, idx) => {
             return (
               <div>
                 <h3>{item.name}</h3>
-                <p>{item.description}</p>
+                <hr />
+                <p className="description">{item.description}</p>
+                {/* <div className="player-wrapper">
+                  <ReactPlayer
+                    controls="true"
+                    className="react-player"
+                    width="100%"
+                    height="469px"
+                    url={item.url}
+                  />
+                </div> */}
               </div>
             );
           })}
@@ -97,10 +138,10 @@ const ProjectPage = ({ item }) => {
       </MediaQuery>
       <MediaQuery maxDeviceWidth={692}>
         <StyledMobilePage>
-          <h1>
+          <h2>
             {item.icon}
             {item.title}
-          </h1>
+          </h2>
           <a href={item.projectLink} target="_blank">
             <p>Checkout The Project</p>
           </a>
@@ -110,14 +151,22 @@ const ProjectPage = ({ item }) => {
           </div>
           <p>{item.skills}</p>
           <img src={item.image1} alt="Main Project Photo" />
-          <h2>Contributions</h2>
-          <hr />
 
           {item.contributions.map((item, idx) => {
             return (
               <div>
                 <h3>{item.name}</h3>
-                <p>{item.description}</p>
+                <hr />
+                {/* <div className="player-wrapper">
+                  <ReactPlayer
+                    className="react-player"
+                    controls="true"
+                    width="95%"
+                    height="240px"
+                    url={item.url}
+                  />
+                </div> */}
+                <p className="description">{item.description}</p>
               </div>
             );
           })}

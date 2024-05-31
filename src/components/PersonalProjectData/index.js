5fe88173427794517a426d01ab29544826a8e31b
Nicholas Taveras <taveras123n@gmail.com>
2024-05-31 12:21:30 -0400

    Updated Personal Project Data to only feature the project title and not the group name

---
+++ src/components/PersonalProjectData/index.js
@@ -0,0 +1,188 @@
+import React from "react";
+import * as GiIcons from "react-icons/gi";
+export const PersonalProjectData = [
+  {
+    title: "Psyche",
+    duration: "August 2023 - December 2023",
+    role: "Game Developer",
+    path: "/portfolio/psyche",
+    skills: "Unity | C# | Jira | Confluence | Perforce | FMOD",
+    projectLink: "https://sadogrestudios.itch.io/psyche",
+    icon: <GiIcons.GiBrain />,
+    description: "Souls-like ARPG, journey into a loved one's psyche.",
+    image1: "/Psyche_Images/psyche_gameplay.png",
+    contributions: [
+      {
+        url: "https://youtu.be/da8y8Lqr_2g",
+        name: "Robust Save and Load System",
+        description:
+          "I implemented a robust save and load system, allowing players to effortlessly save their progress and resume their journey at any point. This feature enhances player convenience and engagement.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "User-Friendly Pause Menu",
+        description:
+          " I designed a user-friendly pause menu, providing players with options to adjust audio and video settings on the fly. This ensures players have control over their gaming experience.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Comprehensive Player Menu",
+        description:
+          "I created a comprehensive player menu that includes class selection, memories, and a quest journal. This menu serves as a valuable resource for players, enhancing their understanding of the game world.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "HUD Design",
+        description:
+          "I designed and implemented a Heads-Up Display (HUD) that displays essential information such as health, stamina, and player abilities, allowing players to stay informed during intense combat scenarios.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Class Swap Wheel UI",
+        description:
+          "I developed a Class Swap Wheel UI that enables players to dynamically switch character classes during combat. This feature adds depth and adaptability to gameplay.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Adaptive Audio Integration",
+        description:
+          " I seamlessly integrated FMOD Studio into the project, enhancing the game with adaptive audio, further immersing players in the game's atmosphere.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Visually Appealing UI:",
+        description:
+          "Utilizing Unity, I designed visually appealing and intuitive user interfaces for both the player menu and pause menu, enhancing the overall user experience.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Quest System Design",
+        description:
+          "Designed and implemented an immersive Quest System, merging quest points, map, and journal for seamless player navigation and organization. Introduced multi-step quests with varied states and rewards, crafting a dynamic narrative that captivates players through real-time notifications.",
+      },
+    ],
+  },
+  {
+    title: "Descender",
+    duration: "May 2023 - May 2023",
+    role: "Game Developer",
+    path: "/portfolio/descender",
+    skills: "C++ | Flecs | Jira | Github | OpenGL | Blender | Freetype",
+    projectLink: "https://phorcy.itch.io/descender-release-build",
+    icon: <GiIcons.GiSpaceship />,
+    description: "Horizontal Scrolling Arcade Game Inspired by Defender.",
+    image1: "/Descender_Images/descender_gameplay.png",
+    contributions: [
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Enhanced Visual Experience",
+        description:
+          "I implemented cutting-edge 3D Orthographic Graphics in OpenGL to elevate the game's visual appeal. This enhancement immerses players in a visually stunning space battleground.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Efficient Text Rendering",
+        description:
+          "Leveraging the power of OpenGL and the freetype library, I ensured efficient and crisp text rendering in the game. Players can easily access vital information, contributing to a seamless gaming experience.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Dynamic UI Elements",
+        description:
+          "Utilizing OpenGL, I created a sophisticated system to render user interface (UI) elements. This system dynamically interpreted position, rotation, and scale data from an XML file, ensuring the UI was seamlessly integrated into the gameplay.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "3D Model Integration",
+        description:
+          " I developed a data-oriented system that seamlessly imported 3D models from Blender-exported objects, expertly converting them into h2b files. This streamlined the integration of 3D assets, contributing to the game's immersive world.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Looping Game World Camera",
+        description:
+          "I implemented a looping game world camera, enhancing the player's navigation experience. This feature ensures a continuous and seamless gameplay environment, allowing players to focus on the action.",
+      },
+    ],
+  },
+  {
+    title: "Thiefs Fortune",
+    duration: "August 2022 - November 2022",
+    role: "Game Developer",
+    path: "/portfolio/thiefs-fortune",
+    skills: "Unity | C# | Trello | Github",
+    projectLink: "https://phorcy.itch.io/thiefs-fortune-demo",
+    icon: <GiIcons.GiPirateFlag />,
+    description: "Third Person Shooter set in the Golden Age of Pirates.",
+    image1: "/ThiefsFortune_Promo/Shop.png",
+    contributions: [
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "AI",
+        description:
+          "I implemented advanced AI behaviors, pathfinding algorithms, and combat tactics to create dynamic and engaging enemy encounters, ensuring that players face formidable adversaries.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Inventory",
+        description:
+          "I managed the game's inventory system, overseeing item spawning, implementing item effects, and streamlining item management. This added depth to the gameplay, allowing players to customize their experience",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Weapon Design",
+        description:
+          "I designed and brought to life a diverse arsenal of weapons, including swords, pistols, and cannons, using Unity and C#. These realistic and meticulously crafted weapons enriched the player's combat experience.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Collaboration",
+        description:
+          "Collaboration was key to our success. I actively collaborated with the development team to ensure seamless and immersive gameplay, including the implementation of player abilities and environmental interactions.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Refinement",
+        description:
+          "Throughout the development process, I tirelessly iterated on game mechanics, ensuring that they were polished to perfection. This dedication resulted in a highly refined and engaging final product.",
+      },
+    ],
+  },
+  {
+    title: "Anubis' Revenge",
+    duration: "August 2022 - September 2022",
+    role: "Game Developer",
+    path: "/portfolio/anubis-revenge",
+    skills: "Unity | C# | Trello | Github",
+    projectLink: "https://phorcy.itch.io/anubis-revenge-demo",
+    icon: <GiIcons.GiMummyHead />,
+    description: "A 2D Platformer, my Unity Debut Project.",
+    image1: "/Anubis_Images/anubis_main.png",
+    contributions: [
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "HUD Development",
+        description:
+          "I designed and implemented the game's Heads-Up Display (HUD), providing players with vital information at a glance. The HUD ensures players stay informed during intense gameplay, enhancing their overall experience.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Menu Systems",
+        description:
+          "I created user-friendly and intuitive menus for pausing the game, winning, and losing. These menus are essential for player interaction and serve as key components in the overall gameplay experience.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Player Controller",
+        description:
+          "I was responsible for crafting all aspects of the player controller, ensuring smooth and responsive character movement, jumping, and action mechanics. The player controller is the core of the game's interactivity and enjoyment.",
+      },
+      {
+        url: "https://www.youtube.com/watch?v=NJuSStkIZBg&t=1546s",
+        name: "Sound Manager Creation",
+        description:
+          "I designed and implemented a comprehensive sound manager responsible for efficiently handling references to all audio assets. This sound manager streamlined the integration of audio into the game, ensuring that sounds played at the right moments and under the appropriate circumstances.",
+      },
+    ],
+  },
+];
